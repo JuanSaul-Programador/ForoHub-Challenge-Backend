0cb3bb95a5ff6794a18fe3e329e7eefb2b829fa9
Juan Saul <Jpereyraacedo87@gmail.com>
2026-02-02 12:32:15 -0500

    Configuración inicial: Proyecto Spring Boot

---
+++ src/test/java/com/juansaul/ForoHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansaul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
